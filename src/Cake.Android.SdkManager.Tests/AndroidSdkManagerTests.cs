3e450ef19b8a24cec4edf4bd46577dc228c0fd2e
Redth <jondick@gmail.com>
2018-04-04 10:59:58 -0400

    More test fixes

--- src/Cake.Android.SdkManager.Tests/AndroidSdkManagerTests.cs
+++ src/Cake.Android.SdkManager.Tests/AndroidSdkManagerTests.cs
@@ -2,54 +2,58 @@
 using Cake.Core.IO;
 using Cake.AndroidSdkManager.Fakes;
 using Cake.AndroidSdkManager;
-using NUnit.Framework;
+using Xunit;
 using Cake.Core;
 using Cake;
 using System.Linq;
 
 namespace Cake.AndroidSdkManager.Tests
 {
-	public class Tests : TestFixtureBase
-	{
-		const string ANDROID_SDK_ROOT = "../../../android-sdk/";
+    public class Tests : TestFixtureBase
+    {
+        const string ANDROID_SDK_ROOT = "../../../android-sdk/";
 
-		[Test] 
-		public void A1_Download_SDK ()
-		{
-			Cake.AndroidSdkManagerDownload(null, null);
+        [Fact]
+        public void A1_Download_SDK()
+        {
+            Cake.AndroidSdkManagerDownload(null, null);
 
-			Assert.IsTrue(Cake.FileSystem.Exist(new FilePath ("./tools/androidsdk/tools/bin/sdkmanager"))
-			             || Cake.FileSystem.Exist(new FilePath("./tools/androidsdk/tools/bin/sdkmanager.bat")));
-		}
-		[Test]
-		public void List()
-		{
-			var list = this.Cake.AndroidSdkManagerList(new AndroidSdkManagerToolSettings {
-				SdkRoot = ANDROID_SDK_ROOT, SkipVersionCheck = false
-			});
+            Assert.True(Cake.FileSystem.Exist(new FilePath("./tools/androidsdk/tools/bin/sdkmanager"))
+                         || Cake.FileSystem.Exist(new FilePath("./tools/androidsdk/tools/bin/sdkmanager.bat")));
+        }
 
-			Assert.NotNull(list);
+        [Fact]
+        public void List()
+        {
+            var list = this.Cake.AndroidSdkManagerList(new AndroidSdkManagerToolSettings
+            {
+                SdkRoot = ANDROID_SDK_ROOT,
+                SkipVersionCheck = false
+            });
 
-			foreach (var a in list.AvailablePackages)
-				Console.WriteLine($"{a.Description}\t{a.Version}\t{a.Path}");
+            Assert.NotNull(list);
 
-			foreach (var a in list.InstalledPackages)
-				Console.WriteLine($"{a.Description}\t{a.Version}\t{a.Path}");
-		}
+            foreach (var a in list.AvailablePackages)
+                Console.WriteLine($"{a.Description}\t{a.Version}\t{a.Path}");
 
-		[Test]
-		public void Install()
-		{
-			var settings = new AndroidSdkManagerToolSettings {
-                SkipVersionCheck=  true,
+            foreach (var a in list.InstalledPackages)
+                Console.WriteLine($"{a.Description}\t{a.Version}\t{a.Path}");
+        }
+
+        [Fact]
+        public void Install()
+        {
+            var settings = new AndroidSdkManagerToolSettings
+            {
+                SkipVersionCheck = true,
                 SdkRoot = "/Users/redth/Library/Developer/Xamarin/android-sdk-macosx/"
-			};
+            };
 
-            this.Cake.AndroidSdkManagerInstall(new [] { "system-images;android-26;google_apis;x86" }, settings);
+            this.Cake.AndroidSdkManagerInstall(new[] { "system-images;android-26;google_apis;x86" }, settings);
 
-			var list = Cake.AndroidSdkManagerList(settings);
+            var list = Cake.AndroidSdkManagerList(settings);
 
-			Assert.NotNull(list.InstalledPackages.FirstOrDefault(ip => ip.Path == "extras;google;auto"));
-		}
-	}
+            Assert.NotNull(list.InstalledPackages.FirstOrDefault(ip => ip.Path == "extras;google;auto"));
+        }
+    }
 }
