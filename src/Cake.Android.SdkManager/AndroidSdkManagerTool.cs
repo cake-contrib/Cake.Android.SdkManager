82c528c202a68d341d30e5f8091942317b3f7b1b
Shane Neuville <shneuvil@microsoft.com>
2020-06-03 18:15:19 -0600

    Timing issue with pipe

--- src/Cake.Android.SdkManager/AndroidSdkManagerTool.cs
+++ src/Cake.Android.SdkManager/AndroidSdkManagerTool.cs
@@ -245,8 +245,19 @@ namespace Cake.AndroidSdkManager
 
 			while (!pex.Complete.IsCompleted)
 			{
-				System.Threading.Thread.Sleep(250);
-				pex.StandardInput.WriteLine("y");
+                System.Threading.Thread.Sleep(250);
+				if (pex.Complete.IsCompleted)
+					break;
+
+				try
+				{
+					pex.StandardInput.WriteLine("y");
+				}
+				catch(Exception exc)
+                {
+					if (exc.Message != "Broken pipe")
+						throw;
+                }
 			}
 
 			pex.Complete.Wait();
